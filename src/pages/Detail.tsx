68ef02d76a74bb837594ef2ea3f9e850a59feecd
devJerb <gutierrez.jeromebryant@gmail.com>
2025-02-05 08:45:07 +0800

    fix: disable video access

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -2,7 +2,13 @@ import Bits from "../assets/videos/bits.mp4";
 
 const Detail = () => {
   return (
-    <video autoPlay muted loop>
+    <video 
+      autoPlay 
+      muted 
+      loop 
+      onContextMenu={(e) => e.preventDefault()}
+      controlsList="nodownload"
+    >
       <source src={Bits} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
